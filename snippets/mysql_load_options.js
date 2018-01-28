309b5415b27b8177914767ea5aecaa3a738e596b
Gabriele Gentile <gabriele.gentile123@gmail.com>
2018-01-28 17:30:26 +0900

    Change: JSON filename

--- snippets/mysql_load_options.js
+++ snippets/mysql_load_options.js
@@ -2,7 +2,7 @@ const fs=require('fs');
 
 // Returns SQL data & login credentials
 function sql_load(){
-    fs.readFile('sql_options.JSON',function (err,data){
+    fs.readFile('sql_options.json',function (err,data){
         if (err){
             throw err;
         } else {
