144e6ef3aadb2e1b7f2cac11be0db0743a72961c
Dragonadi <a.costachel@icloud.com>
2018-02-18 15:55:16 +0900

    Add: Determine path and HTTP method

--- snippets/server-promise/processRequest.js
+++ snippets/server-promise/processRequest.js
@@ -1,12 +1,23 @@
 async function processRequest(request){
 	try{
+		// Determine path
+		var path = request.url;
+
+		// Determine HTTP method
+		var method = request.method;
+		
+		// Extract POST body
+		await new Promise(function(resolve, reject){
+			// Process chunk extraction
+		});
+
 		await requestFormatter;
 		await dataProcessing;
 		await responseFormatter;
 		
-	} catch (errorCode){
+	} catch (errorCode) {
 
-	}finally{
+	} finally {
 		await processResponse;
 	}
 
