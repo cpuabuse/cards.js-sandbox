2353f02846f5047579fe702b52b70d1a43c3ec51
Dragonadi <a.costachel@icloud.com>
2018-02-18 14:42:18 +0900

    Change: Request formatter outline

--- snippets/server-promise/requestFormatter.js
+++ snippets/server-promise/requestFormatter.js
@@ -8,7 +8,39 @@ function requestFormatter(rawData){
 	})
 }
 
-requestFormatter_TSV();
-requestFormatter_Int();
-requestFormatter_JSON();
-requestFormatter_String();
+function requestFormatter_TSV(rawData){
+	return new Promise(function(resolve, reject){
+	
+		resolve(javascriptObject)
+	
+		// if error BAD REQUEST
+	reject(400);
+    })
+}
+function requestFormatter_Int(rawData){
+	return new Promise(function(resolve, reject){
+
+		resolve(javascriptObject)
+
+		// if error BAD REQUEST
+		reject(400);		
+	})
+}
+function requestFormatter_JSON(rawData){
+	return new Promise(function(resolve, reject){
+		
+		resolve(javascriptObject)
+
+		//if error BAD REQUEST
+		reject(400);
+	})
+}
+function requestFormatter_String(rawData){
+	return new Promise(function(resolve){
+
+		resolve(javascriptObject)
+
+		//if error BAD REQUEST
+		reject(400);
+	})
+}
