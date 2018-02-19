cc5d789d8a63f20cd9a9f01608de0edb5a7dcd97
laura-van-der-booben <35882542+laura-van-der-booben@users.noreply.github.com>
2018-02-19 13:39:07 +0900

    Add: Process request logic

--- snippets/server-promise/processRequest.js
+++ snippets/server-promise/processRequest.js
@@ -3,22 +3,59 @@ async function processRequest(request){
 		// Determine path
 		var path = request.url;
 
+		// Throw error if url not matched
+		// IF path not in array throw bad request
+
+		// Match url
+		// Get the url with the system function
+
 		// Determine HTTP method
 		var method = request.method;
+
+		// Throw error if method is not matched
+		// If method not in allowed methods throw 405 Method Not Allowed
+
+		// Determine the functions to be used
+		// We have some array or something, where we extract the functions to use based on method and path
 		
 		// Extract POST body
-		await new Promise(function(resolve, reject){
-			// Process chunk extraction
-		});
+		if (method == 'post'){
+			await new Promise(function(resolve, reject){
+				// Hold POST request data
+				var req_postData = '';
 
-		await requestFormatter;
-		await dataProcessing;
-		await responseFormatter;
+				/* Handling http.IncomingMessage events of additional to stream.Readable */
+				// Event: 'close'
+
+				// Event: 'data' - Get the data from body
+				request.on('data', function (data) {
+					req_postData += data;
+					
+					// We do have some post limit, actually let it be 64KB
+					// if size of req_postData exceeds 64KB reject with 413 Payload Too Large
+				});
+
+				// Event: 'end' - When the transmission ended
+				request.on('end', function () {
+					resolve(req_postData);
+				});
+
+				// Event: 'error'
+				// Event: 'readable'
+
+				/* Handling http.IncomingMessage events inherited from stream.Readable */
+				// Event: 'aborted'
+				// Event: 'close'
+			});
+		}
 		
-	} catch (errorCode) {
-
+		var formattedData = await requestFormatter();
+		var processedData = await dataProcessing(formattedData);
+		var responseData = await responseFormatter(processedData);
+		
+	} catch (thrownErrorCode) {
+		errorCode = thrownErrorCode;
 	} finally {
-		await processResponse;
+		await processResponse({responseData, errorCode});
 	}
-
 }
