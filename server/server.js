cc03e8ab9f4de3596eb666e7fe8bd7594bfdffc6
laura-van-der-booben <35882542+laura-van-der-booben@users.noreply.github.com>
2018-02-20 12:42:12 +0900

    Add: Processing to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 //	server/server.js
 const http = require('http');
+const https = require('https');
 
 exports.server = function(){
-	
+	// TODO: Make somehow an event or something to die, if system error level is too high
+
+
 	const server = http.createServer(function (request, response) {
 		// TODO: Reject request if systemErrorLevelHigh
 		
@@ -35,4 +38,74 @@ exports.server = function(){
 	// Event: 'connection'
 	// Event: 'error'
 	// Event: 'listening'
+}
+
+// Processes the request
+async function processRequest(request, response){
+	try{
+		// Determine path
+		var path = request.url;
+
+		// Throw error if url not matched
+		// TODO: IF path not in array throw bad request
+
+		// Match url
+		// TODO: Get the url with the system function
+
+		// Determine HTTP method
+		var method = request.method;
+
+		// Throw error if method is not matched
+		// TODO: If method not in allowed methods throw 405 Method Not Allowed
+
+		// Determine the functions to be used
+		// TODO: We have some array or something, where we extract the functions to use based on method and path
+
+		var responseData = "hello";
+		
+		// Extract POST body
+		if (method == 'post'){
+			await new Promise(function(resolve, reject){
+				// Hold POST request data
+				var req_postData = '';
+
+				/* Handling http.IncomingMessage events of additional to stream.Readable */
+				// Event: 'close'
+
+				// Event: 'data' - Get the data from body
+				request.on('data', function (data) {
+					req_postData += data;
+					
+					// We do have some post limit, actually let it be 64KB
+					// if size of req_postData exceeds 64KB reject with 413 Payload Too Large
+				});
+
+				// Event: 'end' - When the transmission ended
+				request.on('end', function () {
+					resolve(req_postData);
+				});
+
+				// Event: 'error'
+				// Event: 'readable'
+
+				/* Handling http.IncomingMessage events inherited from stream.Readable */
+				// Event: 'aborted'
+				// Event: 'close'
+			});
+		}
+		/*
+		var formattedData = await requestFormatter();
+		var processedData = await dataProcessing(formattedData);
+		var responseData = await responseFormatter(processedData);
+		*/
+	} catch (thrownErrorCode) {
+		errorCode = thrownErrorCode;
+		responseData = "";
+	} finally {
+		// Set headers
+		// TODO: ??
+
+		// End response
+		response.end(responseData);
+	}
 }
