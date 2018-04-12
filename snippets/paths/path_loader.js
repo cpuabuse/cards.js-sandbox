df6e21594f93a093cdc5372d66037f62a52691aa
Gabriele Gentile <gabriele.gentile123@gmail.com>
2018-04-12 21:31:13 +0900

    Add: Path to resource snippet #3

---
+++ snippets/paths/path_loader.js
@@ -0,0 +1,28 @@
+var resources={
+    index:{
+        main : "index.html"
+    },
+    resource1:{}
+}
+
+var resourceArray={
+    "/resource1" : "resource1",
+    "/resource2" : "resource2",
+    "/" : "index"
+};
+
+// Returns the resource object according to user input path
+var getResourceByPath = function (path, resourceArray){
+    // Checks user input
+    if (!resourceArray.hasOwnProperty(path)) {
+        throw 404;
+    }
+    
+    // Checks internal application integrity
+    var resourceKey = resourceArray[path];
+    if (!resources.hasOwnProperty(resourceKey)){
+        throw 500;
+    }
+    
+    return resources[resourceKey];
+}
